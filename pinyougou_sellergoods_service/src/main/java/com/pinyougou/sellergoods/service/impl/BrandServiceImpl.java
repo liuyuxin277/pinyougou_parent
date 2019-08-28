97fa93380c29c881925e38e96ecbb7ed5f75c557
zhangsan <1656726111@qq.com>
2019-08-28 08:50:20 +0800

    第二天

---
+++ pinyougou_sellergoods_service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,25 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import java.util.List;
+
+import com.alibaba.dubbo.config.annotation.Service;
+
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.sellergoods.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+@Service
+public class BrandServiceImpl implements BrandService {
+
+	@Autowired
+	private TbBrandMapper brandMapper;
+	
+	@Override
+	public List<TbBrand> findAll() {
+
+		return brandMapper.selectByExample(null);
+	}
+
+}
